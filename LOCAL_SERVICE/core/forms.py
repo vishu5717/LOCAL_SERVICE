a9eb7380c307522f3d154b7b51dc1a8fc080a079
vishva patel <vishvapatel17325@gmail.com>
2026-02-23 19:10:18 +0530

    create project + signup form + userbuilt admin page

---
+++ LOCAL_SERVICE/core/forms.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+from django import forms
+
+class UserSignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['email', 'role', 'password1', 'password2']
+        widgets = {
+            'email': forms.EmailInput(attrs={'placeholder': 'Email'}),
+            'role': forms.Select(),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({'placeholder': 'Password'})
+        self.fields['password2'].widget.attrs.update({'placeholder': 'Confirm Password'})
---
+++ LOCAL_SERVICE/core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render,redirect
+from .forms import UserSignupForm
+
+# Create your views here.
+def userSignupView(request):
+    if request.method =="POST":
+      form = UserSignupForm(request.POST or None)
+      if form.is_valid():
+        form.save()
+        return redirect('login') #error
+      else:
+        return render(request,'core/signup.html',{'form':form})  
+    else:
+        form = UserSignupForm()
+        return render(request,'core/signup.html',{'form':form})
